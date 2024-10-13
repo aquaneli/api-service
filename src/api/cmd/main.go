fdbbb08d81c955ba4e5d019b932fdef116d12c0b
Aqua Nelida <aquaneli@si-b3.kzn.21-school.ru>
2024-10-13 12:36:41 +0300

    clean

--- src/api/cmd/main.go
+++ src/api/cmd/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/json"
 	"net/http"
-	"reflect"
 
 	"github.com/labstack/echo"
 )
@@ -18,71 +17,75 @@ var items []Items
 
 func main() {
 	e := echo.New()
-
 	group := e.Group("/item")
 	group.GET("/:caption", GetHandler)
-	group.POST("/cap", PostHandler)
-
+	group.POST("", PostHandler)
 	e.Logger.Fatal(e.Start(":8080"))
 }
 
 func GetHandler(e echo.Context) error {
 	param := e.Param("caption")
+	e.Response().Header().Set(echo.HeaderContentType, echo.MIMEApplicationJSONCharsetUTF8)
 	for _, v := range items {
 		if v.caption == param {
-			return e.JSON(http.StatusOK, v)
+			return e.JSON(http.StatusOK, map[string]interface{}{
+				"caption": v.caption,
+				"weight":  v.weight,
+				"number":  v.number,
+			})
 		}
 	}
-	return e.String(http.StatusInternalServerError, "{\n    \"status_err\": 500\n}")
-}
-
-func GetData() {
-
+	return e.JSONPretty(http.StatusInternalServerError, map[string]interface{}{
+		"status": http.StatusInternalServerError,
+	}, "  ")
 }
 
 func PostHandler(e echo.Context) error {
 	item, err := Parsing(e)
+	e.Response().Header().Set(echo.HeaderContentType, echo.MIMEApplicationJSONCharsetUTF8)
 	if err != nil {
-		return e.JSON(http.StatusInternalServerError, "{\n\"status_err\": 400\n}")
+		return e.JSONPretty(http.StatusInternalServerError, map[string]interface{}{
+			"status": http.StatusBadRequest,
+		}, "  ")
 	}
 	items = append(items, item)
-	return e.JSON(http.StatusInternalServerError, "{\n\"status_ok\": 200\n}")
+	return e.JSONPretty(http.StatusInternalServerError, map[string]interface{}{
+		"status": http.StatusOK,
+	}, "  ")
 }
 
 func Parsing(e echo.Context) (Items, error) {
 	decoder := json.NewDecoder(e.Request().Body)
-	decoder.Token()
+	_, err := decoder.Token()
+	if err != nil {
+		return Items{}, err
+	}
 	item := Items{}
 	for decoder.More() {
 		key, err := decoder.Token()
 		if err != nil {
 			return Items{}, err
 		}
-		value, err := decoder.Token()
-		if err != nil {
-			return Items{}, err
-		}
 
 		switch key {
 		case "caption":
-			val := reflect.ValueOf(value)
-			item.caption = val.String()
+			err := decoder.Decode(&item.caption)
+			if err != nil {
+				return Items{}, err
+			}
 		case "weight":
-			val := reflect.ValueOf(value)
-			ok := val.CanFloat()
-			if !ok {
+			err := decoder.Decode(&item.weight)
+			if err != nil {
 				return Items{}, err
 			}
-			item.weight = float32(val.Float())
 		case "number":
-			val := reflect.ValueOf(value)
-			ok := val.CanInt()
-			if !ok {
+			err := decoder.Decode(&item.number)
+			if err != nil {
 				return Items{}, err
 			}
-			item.number = int(val.Int())
 		}
 
 	}
+
 	return item, nil
 }
